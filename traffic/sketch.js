44463da41fe8328d00bf010c8b83bf497e17fd4a
yomarisv <yomaris.verdin@ms223.org>
2016-10-14 14:26:52 -0400

    Add files via upload

---
+++ traffic/sketch.js
@@ -0,0 +1,12 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+  fill('grey');
+  rect(200,200,20,200);
+  fill('yellow');
+  rect(170,150,80,130);
+  ellipse(170,80,20,20);
+}
